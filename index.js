1b086e04b2f942ff86187ddbb9dd00f72756a42d
Janak J Prajapati <133052681+Janak342005@users.noreply.github.com>
2023-11-20 19:01:11 +0000

    JS-Practice set 1 finish

---
+++ index.js
@@ -0,0 +1,25 @@
+// JavaScript ractice set 1
+// Print numbers from 1 to 100 ---- I changed numbers from this to from 1 to 10 since we have to do it in all three loops for loop do while loop and while loop
+
+let a = 0;
+let b = 0;
+
+console.log("for loop starts here");
+
+for(let j = 0; j <= 10; j++){
+    console.log(j);
+}
+
+console.log("do while loop starts here");
+
+do {
+    console.log(a);
+    a++
+} while (a <= 10);
+
+console.log("while loop");
+
+while (b <= 10) {
+    console.log(b);
+    b++;
+}
